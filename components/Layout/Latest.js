7d066e1b3009d9322a0a874b9e20f4b26402d8e4
Odumosu Bolaji <odmusbolaji31@gmail.com>
2025-01-16 08:08:56 +0100

    updating latest

--- components/Layout/Latest.js
+++ components/Layout/Latest.js
@@ -3,13 +3,13 @@ import sampleProducts from './sampleProducts';
 
 export default function Latest({ products = sampleProducts }) {
     return (
-      <div className="w-3/6 mx-auto py-12">
+      <div className="w-full p-4 md:p-0 lg:w-3/6 mx-auto py-12">
         <div className="mb-8">
           <div className="text-md  text-gray-500">Explore Our Products</div>
           <h2 className="text-2xl  text-gray-900">Latest Products</h2>
         </div>
         <div className="flex flex-wrap">
-          <div role="list" className="flex justify-between w-full">
+          <div role="list" className="grid grid-cols-1 md:grid-cols-3 w-full">
             {products.length > 0 ? (
                 products.map((product) => (
                     <LatestItem key={product.id} product={product} />
