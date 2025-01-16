6e2f7b5cda27a5a2b0eb05d2f2e7c4c680ee4304
Odumosu Bolaji <odmusbolaji31@gmail.com>
2025-01-16 07:48:46 +0100

    navbar updated

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,37 +1,55 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Navbar = (props) => {
+
+  const [toggle, SetToggle] = useState(false);
+  
   return (
-    <nav className={`w-full border-b ${props.style} border-neutral-800 p-2`}>
-      <div className="w-3/6 mx-auto flex justify-between items-center py-4">
+    <nav className={`w-full border-b ${props.style ? props.style : "bg-white text-black"} border-neutral-800 p-2`}>
+      <div className="w-full px-4 lg:p-0 lg:w-3/6 mx-auto flex justify-between items-center py-4">
         <Link href="/">
-          <Image 
-            src={`${props.imgSrc}`} 
-            alt="Global Times Logo" 
-            width={200} 
-            height={50}
-          />
+            <Image 
+              src={`${props.imgSrc ? props.imgSrc : "/images/GlobalTimesLogoDarkTransparent.svg"}`} 
+              alt="Global Times Logo" 
+              width={200} 
+              height={50}
+            />
         </Link>
         
-        <div className="flex space-x-6 text-xs">
-          <Link href="/index" >Home</Link>
-          <Link href="/about" >About</Link>
-          <Link href="/shop" >Shop</Link>
-          <Link href="/repair" >Repair</Link>
-          <Link href="/contact" >Contact</Link>
+        <div className={`absolute ${toggle ? "flex flex-col transition-transform duration-500 ease-in h-72 top-20" : "hidden h-0 overflow-hidden"} w-full left-0 z-10  justify-center items-center bg-black lg:w-3/6 lg:left-auto 2xl:static 2xl:flex 2xl:flex-row 2xl:space-x-6 2xl:w-auto 2xl:py-4 text-xs`}>
+          <Link href="/"className='py-3' >Home</Link>
+          <Link href="/about" className='py-3'>About</Link>
+          <Link href="/shop" className='py-3'>Shop</Link>
+          <Link href="/repair" className='py-3'>Repair</Link>
+          <Link href="/contact" className='py-3'>Contact</Link>
+
+          <Link href="/" className='w-full py-3 px-4 2xl:hidden' >
+            <div className='py-1 px-5 flex items-center rounded-full w-full border border-neutral-700'>
+              <Image 
+                src="/images/w-ic-search.svg" 
+                alt="Search Icon" 
+                width={22} 
+                height={22} 
+              />
+              <input placeholder='Search...' className='w-full p-2 border-none focus:border-none focus:outline-none text-md bg-black'></input>
+            </div>
+          </Link>
+          
         </div>
         
         <div className="flex items-center space-x-4">
-          <Link href="/log-in">
-            <Image 
-              src="/images/w-ic-search.svg" 
-              alt="Search Icon" 
-              width={22} 
-              height={22} 
-            />
-          </Link>
-          <Link href="/log-in">
+          <Link href="/login" className='2xl:flex hidden'>
+              <Image 
+                src="/images/w-ic-search.svg" 
+                alt="Search Icon" 
+                width={22} 
+                height={22} 
+              />
+            </Link>
+         
+          <Link href="/login">
             <Image 
               src="/images/w-ic-user.svg" 
               alt="User Icon" 
@@ -39,20 +57,33 @@ const Navbar = (props) => {
               height={22} 
             />
           </Link>
-          <div className='flex items-center space-x-3'>
+          <div className='flex items-center sm:space-x-4'>
             <Link href="/cart">
               <Image 
                 src="/images/icon-cart.svg" 
                 alt="Cart Icon" 
                 width={22} 
                 height={22} 
+                className='sm:flex  hidden'
               />
             </Link>
-            <div className='text-xs'>
-                Cart (0)
+            <div className='text-xs flex space-x-1'>
+               <p>Cart </p> 
+               <p>(0)</p> 
             </div>
           </div>
-          
+
+          <div className='2xl:hidden flex'>
+            <Link href="">
+              <Image 
+                src="/images/menu-bar.svg" 
+                alt="Cart Icon" 
+                width={24} 
+                height={24} 
+                onClick={() => SetToggle(!toggle)}
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
